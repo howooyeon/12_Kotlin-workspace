0ad6ae1a0ee8c7d55754752dfb34b8cb6c90aeb9
hoyeon <aviationlovers@naver.com>
2024-06-27 01:43:18 +0900

    6/27 코틀린 기초 문법

---
+++ chapter02/src/Hw02-01.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var name : String = "슈먼이"
+    var ID : Int = 2020110336
+    var age : Short = 24
+    var dept : String = "Software Converence"
+    var gender : Char = 'F'
+
+    println(name)
+    println(ID)
+    println(age)
+    println(dept)
+    println(gender)
+
+}
